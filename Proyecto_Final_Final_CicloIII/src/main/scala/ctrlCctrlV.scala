d0b0464ac7957c471ae221bbf9b141d9c3991fb5
nahomi2004 <soy.nahomi.mucho.gusto@gmail.com>
2024-02-05 16:47:33 -0500

    -aEste es el primer commit >:c denme créditos >:c

---
+++ Proyecto_Final_Final_CicloIII/src/main/scala/ctrlCctrlV.scala
@@ -0,0 +1,88 @@
+// import para el archivo
+import com.github.tototoshi.csv.*
+import java.io.File
+
+// escribir los datos en un txt
+import java.io.{BufferedWriter, FileWriter}
+
+import doobie._
+import doobie.implicits._
+
+import cats._
+import cats.effect._
+import cats.effect.unsafe.implicits.global
+import cats.implicits._
+
+implicit object CustomFormat extends  DefaultCSVFormat {
+  override  val delimiter: Char = ';'
+}
+
+object ctrlCctrlV {
+  @main
+  def ejecutable() =
+
+    val xa = Transactor.fromDriverManager[IO](
+      driver = "com.mysql.cj.jdbc.Driver",
+      url = "jdbc:mysql://localhost: 3306/mundial",
+      user = "root",
+      password = "ilovelasranas2",
+      logHandler = None)
+
+    val ruta = "C:/Users/D E L L/Documents/Nahomi/CICLO III/PFR/ArchivoPIntegrador/dsAlineacionesXTorneo.csv"
+    val reader = CSVReader.open(new File(ruta))
+    val contentFile: List[Map[String, String]] =
+      reader.allWithHeaders()
+
+
+    def valoresDoBuedos(valor: String) =
+      if (valor == "not available" || valor == "not applicable" || valor == "NA" || valor == "\\s") {
+        0
+      } else {
+        valor.toDouble
+      }
+
+    def escribirDatosTXT (nombreTXT: String, archivo: String): Unit =
+      val rutaTXT = "C:/Users/D E L L/Documents/Nahomi/CICLO III/PROYECTO FINAL FINAL FINAL/"
+      val rutaFinal = rutaTXT + nombreTXT
+
+      val escritor = new BufferedWriter(new FileWriter(rutaFinal, true))
+      try {
+        escritor.write(archivo)
+        escritor.newLine()
+      } finally {
+        escritor.close()
+      }
+
+    def generateDataSquadsTable(data: List[Map[String, String]]): Unit = // en esta función crearemos los insert into o lo que sea necesario para poblar la tabla Genre
+      val nombreTXT = "squads.txt"
+      val insertFormat = s"INSERT INTO squads(squads_player_id, squads_tournament_id, squads_team_id, squads_shirt_number, squads_position_name) VALUES('%s', '%s', '%s', %d, '%s');"
+      val value = data
+        .map(x => (x("squads_player_id"), x("squads_tournament_id"), x("squads_team_id"), valoresDoBuedos(x("squads_shirt_number")).toInt, x("squads_position_name")))
+        .sortBy(x => (x._1, x._2))
+        .map(x => escribirDatosTXT(nombreTXT, insertFormat.formatLocal(java.util.Locale.US, x._1, x._2, x._3, x._4, x._5)))
+
+      // value.foreach(println)
+
+
+      /*
+      println(data.map(x => x("squads_shirt_number") == "not applicable").
+        filter(_ == true))
+
+      Esto solo era para comprobar si no había ningun valor extraño, nada más
+       */
+
+    def generateDataPlayersTable (data: List[Map[String, String]]): Unit =
+      val nombreTXT = "players.txt"
+      val insertFormat = s"INSERT INTO players(squadsPlayerId, squadsTournamentId, players_given_name, players_family_name, players_birth_date, players_female, players_goal_keepere, players_defender, players_midfielder, players_forward) VALUES('%s', '%s', '%s', '%s', '%s', %d, %d, %d, %d, %d);"
+      val value = data
+        .map(x => (x("squads_player_id"), x("squads_tournament_id"), x("players_given_name"), x("players_family_name"), x("players_birth_date"), valoresDoBuedos(x("players_female")).toInt, valoresDoBuedos(x("players_goal_keeper")).toInt, valoresDoBuedos(x("players_defender")).toInt, valoresDoBuedos(x("players_midfielder")).toInt, valoresDoBuedos(x("players_forward")).toInt))
+        .sortBy(x => (x._3, x._5))
+        .map(x => escribirDatosTXT(nombreTXT, insertFormat.formatLocal(java.util.Locale.US, x._1, x._2, x._3, x._4, x._5, x._6, x._7, x._8, x._9, x._10)))
+
+      //value.foreach(println)
+
+
+    // llamar a los métodos
+    generateDataSquadsTable(contentFile)
+    generateDataPlayersTable(contentFile)
+}
---
+++ proyecto_integrador.sql
@@ -0,0 +1,110 @@
+CREATE DATABASE mundial;
+USE mundial;
+
+CREATE TABLE squads(
+  squads_player_id VARCHAR(15) NOT NULL,
+  squads_tournament_id VARCHAR(15) NOT NULL,
+  squads_team_id VARCHAR(15) NOT NULL,
+  squads_shirt_number INT NOT NULL,
+  squads_position_name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (squads_player_id, squads_tournament_id)
+);
+
+CREATE TABLE players(
+  squadsPlayerId VARCHAR(15) NOT NULL, 
+  squadsTournamentId VARCHAR(15) NOT NULL, 
+  
+  players_given_name VARCHAR(30) NOT NULL,
+  players_family_name VARCHAR(30) NOT NULL,
+  players_birth_date VARCHAR(50) NOT NULL,
+  players_female INT NOT NULL,
+  players_goal_keepere VARCHAR(20) NOT NULL,
+  players_defender INT NOT NULL,
+  players_midfielder INT NOT NULL,
+  players_forward INT NOT NULL,
+  PRIMARY KEY (players_given_name, players_birth_date),
+  FOREIGN KEY (squadsPlayerId, squadsTournamentId) REFERENCES squads(squads_player_id, squads_tournament_id)
+);
+-- DROP TABLE players;
+
+CREATE TABLE tournaments(
+  tournaments_year INT NOT NULL, 
+  tournaments_tournament_name VARCHAR(40) NOT NULL,
+  tournaments_host_country VARCHAR(20) NOT NULL,
+  tournaments_winner VARCHAR(20) NOT NULL,
+  tournaments_count_teams INT NOT NULL,
+  PRIMARY KEY (tournaments_year)
+);
+
+CREATE TABLE stadiums(
+  stadiums_stadium_name VARCHAR(50) NOT NULL, 
+  stadiums_city_name VARCHAR(20) NOT NULL,
+  stadiums_country_name VARCHAR(20) NOT NULL,
+  stadiums_stadium_capacity VARCHAR(10) NOT NULL,
+  PRIMARY KEY (stadiums_stadium_name)
+);
+
+CREATE TABLE matches(
+  tournamentsYear INT NOT NULL,
+  stadiumsStadiumName VARCHAR(50) NOT NULL,
+
+  matches_tournament_id VARCHAR(15) NOT NULL, 
+  matches_match_id VARCHAR(10) NOT NULL,
+  matches_away_team_id VARCHAR(5) NOT NULL,
+  matches_home_team_id VARCHAR(5) NOT NULL,
+  matches_stadium_id VARCHAR(5) NOT NULL,
+  matches_match_date VARCHAR(50) NOT NULL,
+  matches_match_time VARCHAR(20) NOT NULL,
+  matches_stage_name VARCHAR(30) NOT NULL,
+  matches_home_team_score INT NOT NULL,
+  matches_away_team_score INT NOT NULL,
+  matches_extra_time INT NOT NULL,
+  matches_penalty_shootout INT NOT NULL,
+  matches_home_team_score_penalties INT NOT NULL,
+  matches_away_team_score_penalties INT NOT NULL,
+  matches_result VARCHAR(20) NOT NULL,
+  PRIMARY KEY (matches_tournament_id, matches_match_id),
+  FOREIGN KEY (tournamentsYear) REFERENCES tournaments(tournaments_year),
+  FOREIGN KEY (stadiumsStadiumName) REFERENCES stadiums(stadiums_stadium_name)
+);
+
+CREATE TABLE teams(
+  squadsPlayerId VARCHAR(15) NOT NULL,
+  squadsTournamentId VARCHAR(15) NOT NULL,
+
+  home_team_name VARCHAR(30) NOT NULL, 
+  away_team_name VARCHAR(30) NOT NULL,
+  home_mens_team INT NOT NULL,
+  home_womens_team INT NOT NULL,
+  home_region_name VARCHAR(20) NOT NULL,
+  away_mens_team INT NOT NULL,
+  away_womens_team INT NOT NULL,
+  away_region_name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (home_team_name, away_team_name),
+  FOREIGN KEY (squadsPlayerId, squadsTournamentId) REFERENCES squads(squads_player_id, squads_tournament_id)
+);
+
+CREATE TABLE goals(
+  playersGivenName VARCHAR(30) NOT NULL, 
+  playersBirthDate VARCHAR(50) NOT NULL,  
+  matchesTournamentId VARCHAR(15) NOT NULL, 
+  homeTeamName VARCHAR(30) NOT NULL, 
+  awayTeamName VARCHAR(30) NOT NULL, 
+  tournamentsYear INT NOT NULL, 
+  
+  goals_goal_id VARCHAR(15) NOT NULL,
+  goals_team_id VARCHAR(15) NOT NULL,
+  goals_player_id VARCHAR(15) NOT NULL,
+  goals_player_team_id VARCHAR(15) NOT NULL,
+  goals_minute_label VARCHAR(15) NOT NULL,
+  goals_minute_regulation INT NOT NULL,
+  goals_minute_stoppage INT NOT NULL,
+  goals_match_period VARCHAR(50),
+  goals_own_goal INT NOT NULL,
+  goals_penalty INT NOT NULL,
+  PRIMARY KEY (goals_goal_id),
+  FOREIGN KEY (playersGivenName, playersBirthDate) REFERENCES players(players_given_name, players_birth_date),
+  FOREIGN KEY (matchesTournamentId) REFERENCES matches(matches_tournament_id),
+  FOREIGN KEY (homeTeamName, awayTeamName) REFERENCES teams(home_team_name, away_team_name),
+  FOREIGN KEY (tournamentsYear) REFERENCES tournaments(tournaments_year)
+);
