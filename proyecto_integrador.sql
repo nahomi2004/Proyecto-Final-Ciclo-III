5cd95a5171ba3d77989abc73dd92e00d8bbcfdc2
nahomi2004 <soy.nahomi.mucho.gusto@gmail.com>
2024-02-07 17:28:46 -0500

    UWU ACABÉ LA BASE DE DATOS

--- proyecto_integrador.sql
+++ proyecto_integrador.sql
@@ -7,7 +7,10 @@ CREATE TABLE squads(
   squads_team_id VARCHAR(15) NOT NULL,
   squads_shirt_number INT NOT NULL,
   squads_position_name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (squads_player_id, squads_tournament_id)
+  PRIMARY KEY (squads_player_id, squads_tournament_id),
+  
+  INDEX idx_squads_tournament_id (squads_tournament_id),
+  INDEX idx_squads_player_id (squads_player_id)
 );
 
 CREATE TABLE players(
@@ -18,12 +21,14 @@ CREATE TABLE players(
   players_family_name VARCHAR(30) NOT NULL,
   players_birth_date VARCHAR(50) NOT NULL,
   players_female INT NOT NULL,
-  players_goal_keepere INT NOT NULL,
+  players_goal_keeper INT NOT NULL,
   players_defender INT NOT NULL,
   players_midfielder INT NOT NULL,
   players_forward INT NOT NULL,
+  PRIMARY KEY (players_given_name, players_family_name, squadsTournamentId, squadsPlayerId),
+  
   FOREIGN KEY (squadsPlayerId, squadsTournamentId) REFERENCES squads(squads_player_id, squads_tournament_id),
-  PRIMARY KEY (players_given_name, players_family_name, squadsTournamentId, squadsPlayerId)
+  INDEX idx_squadsTournamentId_squadsPlayerId (squadsTournamentId, squadsPlayerId)
 );
 -- DROP TABLE players;
 
@@ -41,7 +46,8 @@ CREATE TABLE stadiums(
   stadiums_city_name VARCHAR(20) NOT NULL,
   stadiums_country_name VARCHAR(20) NOT NULL,
   stadiums_stadium_capacity INT NOT NULL,
-  PRIMARY KEY (stadiums_stadium_name)
+  PRIMARY KEY (stadiums_stadium_name, stadiums_city_name),
+  INDEX idx_stadiums_stadium_name (stadiums_stadium_name)
 );
 
 CREATE TABLE matches(
@@ -62,14 +68,14 @@ CREATE TABLE matches(
   matches_penalty_shootout INT NOT NULL,
   matches_home_team_score_penalties INT NOT NULL,
   matches_away_team_score_penalties INT NOT NULL,
-  matches_result VARCHAR(20) NOT NULL,
+  matches_result VARCHAR(30) NOT NULL,
   PRIMARY KEY (matches_match_id),
+  
   FOREIGN KEY (tournamentsYear) REFERENCES tournaments(tournaments_year),
   FOREIGN KEY (stadiumsStadiumName) REFERENCES stadiums(stadiums_stadium_name)
 );
 
 CREATE TABLE teams(
-  squadsPlayerId VARCHAR(15) NOT NULL,
   squadsTournamentId VARCHAR(15) NOT NULL,
 
   home_team_name VARCHAR(30) NOT NULL, 
@@ -80,15 +86,16 @@ CREATE TABLE teams(
   away_mens_team INT NOT NULL,
   away_womens_team INT NOT NULL,
   away_region_name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (home_team_name, away_team_name),
-  FOREIGN KEY (squadsPlayerId, squadsTournamentId) REFERENCES squads(squads_player_id, squads_tournament_id)
+  PRIMARY KEY (home_team_name, away_team_name, squadsTournamentId),
+  FOREIGN KEY (squadsTournamentId) REFERENCES squads(squads_tournament_id)
 );
 
 CREATE TABLE goals(
-  playersGivenName VARCHAR(30) NOT NULL, 
-  playersFamilyName VARCHAR(30) NOT NULL,
+  -- playersGivenName VARCHAR(30) NOT NULL, 
+  -- playersFamilyName VARCHAR(30) NOT NULL,  
+  -- squadsPlayerIdG VARCHAR(15) NOT NULL,
+  
   squadsTournamentIdG VARCHAR(15) NOT NULL, 
-  squadsPlayerIdG VARCHAR(15) NOT NULL,
   matchesMatchId VARCHAR(10) NOT NULL,
   homeTeamName VARCHAR(30) NOT NULL, 
   awayTeamName VARCHAR(30) NOT NULL, 
@@ -104,9 +111,12 @@ CREATE TABLE goals(
   goals_match_period VARCHAR(50),
   goals_own_goal INT NOT NULL,
   goals_penalty INT NOT NULL,
-  PRIMARY KEY (goals_goal_id),
-  FOREIGN KEY (playersGivenName, playersFamilyName, squadsTournamentIdG, squadsPlayerIdG) REFERENCES players(players_given_name, players_family_name, squadsTournamentId, squadsPlayerId),
+  PRIMARY KEY (goals_goal_id, matchesMatchId),
+  FOREIGN KEY (squadsTournamentIdG) REFERENCES players(squadsTournamentId),
   FOREIGN KEY (matchesMatchId) REFERENCES matches(matches_match_id),
   FOREIGN KEY (homeTeamName, awayTeamName) REFERENCES teams(home_team_name, away_team_name),
   FOREIGN KEY (tournamentsYear) REFERENCES tournaments(tournaments_year)
 );
+
+-- DROP TABLE goals;
+
