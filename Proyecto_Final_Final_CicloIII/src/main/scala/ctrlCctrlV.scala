2d367f2e46af5caff5bfb1d589eb1430777e76d3
nahomi2004 <soy.nahomi.mucho.gusto@gmail.com>
2024-02-07 19:36:40 -0500

    UWU TODO LISTO EL CÓDIGO

--- Proyecto_Final_Final_CicloIII/src/main/scala/ctrlCctrlV.scala
+++ Proyecto_Final_Final_CicloIII/src/main/scala/ctrlCctrlV.scala
@@ -26,7 +26,7 @@ object ctrlCctrlV {
 
     val xa = Transactor.fromDriverManager[IO](
       driver = "com.mysql.cj.jdbc.Driver",
-      url = "jdbc:mysql://localhost: 3306/mundial",
+      url = "jdbc:mysql://localhost: 3306/mundialscala",
       user = "root",
       password = "ilovelasranas2",
       logHandler = None)
@@ -226,33 +226,175 @@ object ctrlCctrlV {
     // -----------------------------------------------------------------------------------------------------------------
     // DATOS PARA INSERTAR DIRECTO
 
-    def generateDataTourments(data: List[Map[String, String]]) =
+    def generateDataSquadsTable(data: List[Map[String, String]]) =
+      // ? -> indicar parámetros de sustitución.
+      val valores = data
+        .map(x => (x("squads_player_id").trim,
+          x("squads_tournament_id").trim,
+          x("squads_team_id").trim,
+          valoresDoBuedos(x("squads_shirt_number").trim).toInt,
+          x("squads_position_name").trim
+        ))
+        .distinct
+        .map(info =>
+          sql"""INSERT INTO squads(squads_player_id, squads_tournament_id, squads_team_id,
+               squads_shirt_number, squads_position_name) VALUES(${info._1}, ${info._2}, ${info._3},
+               ${info._4}, ${info._5})"""
+            .stripMargin
+            .update)
+
+      valores
+
+    def generateDataPlayersTable(data: List[Map[String, String]]) =
+      // ? -> indicar parámetros de sustitución.
+      val valores = data
+        .map(x => (x("squads_player_id").trim,
+          x("squads_tournament_id").trim,
+          comillasRaras(x("players_given_name").trim),
+          comillasRaras(x("players_family_name").trim),
+          comillasRaras(x("players_birth_date").trim),
+          valoresDoBuedos(x("players_female").trim).toInt,
+          valoresDoBuedos(x("players_goal_keeper").trim).toInt,
+          valoresDoBuedos(x("players_defender").trim).toInt,
+          valoresDoBuedos(x("players_midfielder").trim).toInt,
+          valoresDoBuedos(x("players_forward").trim).toInt)
+        )
+        .distinct
+        .map(info =>
+          sql"""INSERT INTO players(squadsPlayerId, squadsTournamentId, players_given_name, players_family_name,
+                players_birth_date, players_female, players_goal_keeper, players_defender, players_midfielder,
+                players_forward) VALUES(${info._1}, ${info._2}, ${info._3}, ${info._4}, ${info._5}, ${info._6},
+                ${info._7}, ${info._8}, ${info._9}, ${info._10})"""
+            .stripMargin
+            .update)
+
+      valores
+
+    def generateDataTournamentsTable(data: List[Map[String, String]]) =
       // ? -> indicar parámetros de sustitución.
       val valores = data
         .map(x => (valoresDoBuedos(x("tournaments_year").trim).toInt,
           x("tournaments_tournament_name").trim,
           x("tournaments_host_country").trim,
           x("tournaments_winner").trim,
-          valoresDoBuedos(x("tournaments_count_teams").trim).toInt))
+          valoresDoBuedos(x("tournaments_count_teams").trim).toInt
+        ))
         .distinct
         .map(info =>
           sql"""INSERT INTO tournaments(tournaments_year, tournaments_tournament_name,
                tournaments_host_country, tournaments_winner, tournaments_count_teams)
-               VALUES(${info._1}, ${info._2}, ${info._3}, ${info._4}, ${info._5})""".stripMargin.update)
+               VALUES(${info._1}, ${info._2}, ${info._3}, ${info._4}, ${info._5})"""
+            .stripMargin
+            .update)
 
       valores
 
-    /*def insertIntoTourmentData(info: (Int, String, String, String, Int)): IO[Int] =
-      val sentencia = sql"""
-             |USE mundial;
-             |INSERT INTO tournaments(tournaments_year, tournaments_tournament_name, tournaments_host_country, tournaments_winner, tournaments_count_teams)
-             |VALUES(${info._1}, ${info._2}, ${info._3}, ${info._4}, ${info._5});
-             |"""
-      sentencia.update.run.transact(xa)*/
+    def generateDataStadiumTable(data: List[Map[String, String]]) =
+      // ? -> indicar parámetros de sustitución.
+      val valores = data
+        .map(x => (comillasRaras(x("stadiums_stadium_name").trim),
+          comillasRaras(x("stadiums_city_name").trim),
+          comillasRaras(x("stadiums_country_name").trim),
+          valoresDoBuedos(x("stadiums_stadium_capacity")).toInt
+        ))
+        .distinct
+        .map(info =>
+          sql"""INSERT INTO stadiums(stadiums_stadium_name, stadiums_city_name, stadiums_country_name,
+                stadiums_stadium_capacity) VALUES(${info._1}, ${info._2}, ${info._3}, ${info._4})"""
+            .stripMargin
+            .update)
 
-    /*def runInsertsIntos() =
-      generateDataTourments(contentFile2).debug.as(ExitCode.Success)*/
+      valores
 
+    def generateDataMatchesTable(data: List[Map[String, String]]) =
+      // ? -> indicar parámetros de sustitución.
+      val valores = data
+        .map(x => (valoresDoBuedos(x("tournaments_year").trim).toInt,
+          comillasRaras(x("stadiums_stadium_name".trim)),
+          comillasRaras(x("matches_tournament_id").trim),
+          comillasRaras(x("matches_match_id").trim),
+          comillasRaras(x("matches_away_team_id").trim),
+          comillasRaras(x("matches_home_team_id").trim),
+          comillasRaras(x("matches_stadium_id").trim),
+          comillasRaras(x("matches_match_date").trim),
+          comillasRaras(x("matches_match_time").trim),
+          comillasRaras(x("matches_stage_name").trim),
+          valoresDoBuedos(x("matches_home_team_score").trim).toInt,
+          valoresDoBuedos(x("matches_away_team_score").trim).toInt,
+          valoresDoBuedos(x("matches_extra_time").trim).toInt,
+          valoresDoBuedos(x("matches_penalty_shootout").trim).toInt,
+          valoresDoBuedos(x("matches_home_team_score_penalties").trim).toInt,
+          valoresDoBuedos(x("matches_away_team_score_penalties").trim).toInt,
+          comillasRaras(x("matches_result").trim)
+        ))
+        .distinct
+        .map(info =>
+          sql"""INSERT INTO matches(tournamentsYear, stadiumsStadiumName, matches_tournament_id, matches_match_id,
+               matches_away_team_id, matches_home_team_id, matches_stadium_id, matches_match_date,
+               matches_match_time, matches_stage_name, matches_home_team_score, matches_away_team_score,
+               matches_extra_time, matches_penalty_shootout, matches_home_team_score_penalties,
+               matches_away_team_score_penalties, matches_result) VALUES(${info._1}, ${info._2}, ${info._3}, ${info._4},
+               ${info._5}, ${info._6}, ${info._7}, ${info._8}, ${info._9}, ${info._10}, ${info._11}, ${info._12},
+               ${info._13}, ${info._14}, ${info._15}, ${info._16}, ${info._17})"""
+            .stripMargin
+            .update)
+
+      valores
+
+    def generateDataTeamsTable(data: List[Map[String, String]]) =
+      // ? -> indicar parámetros de sustitución.
+      val valores = data
+        .map(x => (x("matches_tournament_id").trim,
+          comillasRaras(x("home_team_name").trim),
+          comillasRaras(x("away_team_name").trim),
+          valoresDoBuedos(x("home_mens_team").trim).toInt,
+          valoresDoBuedos(x("home_womens_team").trim).toInt,
+          comillasRaras(x("home_region_name").trim),
+          valoresDoBuedos(x("away_mens_team").trim).toInt,
+          valoresDoBuedos(x("away_womens_team").trim).toInt,
+          comillasRaras(x("away_region_name").trim)
+        ))
+        .distinct
+        .map(info =>
+          sql"""INSERT INTO teams(squadsTournamentId, home_team_name, away_team_name, home_mens_team,
+                home_womens_team, home_region_name, away_mens_team, away_womens_team, away_region_name)
+                VALUES(${info._1}, ${info._2}, ${info._3}, ${info._4}, ${info._5}, ${info._6},
+                ${info._7}, ${info._8}, ${info._9})"""
+            .stripMargin
+            .update)
+
+      valores
+
+    def generateDataGoalsTable(data: List[Map[String, String]]) =
+      // ? -> indicar parámetros de sustitución.
+      val valores = data
+        .map(x => (x("matches_tournament_id").trim,
+          x("matches_match_id").trim,
+          comillasRaras(x("home_team_name").trim),
+          comillasRaras(x("away_team_name").trim),
+          valoresDoBuedos(x("tournaments_year").trim).toInt,
+          comillasRaras(x("goals_goal_id").trim),
+          comillasRaras(x("goals_team_id").trim),
+          comillasRaras(x("goals_player_id").trim),
+          comillasRaras(x("goals_player_team_id").trim),
+          comillasRaras(x("goals_minute_label").trim),
+          valoresDoBuedos(x("goals_minute_regulation").trim).toInt,
+          valoresDoBuedos(x("goals_minute_stoppage").trim).toInt,
+          comillasRaras(x("goals_match_period").trim),
+          valoresDoBuedos(x("goals_own_goal").trim).toInt,
+          valoresDoBuedos(x("goals_penalty").trim).toInt
+        ))
+        .distinct
+        .map(info =>
+          sql"""INSERT INTO goals(squadsTournamentIdG, matchesMatchId, homeTeamName, awayTeamName, tournamentsYear,
+                goals_goal_id, goals_team_id, goals_player_id, goals_player_team_id, goals_minute_label,
+                goals_minute_regulation, goals_minute_stoppage, goals_match_period, goals_own_goal, goals_penalty)
+                VALUES(${info._1}, ${info._2}, ${info._3}, ${info._4}, ${info._5}, ${info._6}, ${info._7}, ${info._8},
+                ${info._9}, ${info._10}, ${info._11}, ${info._12}, ${info._13}, ${info._14}, ${info._15})"""
+            .stripMargin
+            .update)
+
+      valores
 
     // obtenerlistadeJugadores(contentFile)
 
@@ -261,6 +403,7 @@ object ctrlCctrlV {
     // -----------------------------------------------------------------------------------------------------------------
     // MÉTODOS QUE CREAN EL TXT
 
+    /*
     generateDataSquadsTableTXT(contentFile)
     generateDataPlayersTableTXT(contentFile)
     generateDataTournamentsTableTXT(contentFile2)
@@ -268,8 +411,17 @@ object ctrlCctrlV {
     generateDataMatchesTableTXT(contentFile2)
     generateDataTeamsTableTXT(contentFile2)
     generateDataGoalsTableTXT(contentFile2)
+    */
 
     // -----------------------------------------------------------------------------------------------------------------
     // MÉTODOS QUE SE CONECTAN DIRECTO CON LA BASE
-    // generateDataTourments(contentFile2).foreach(i => i.run.transact(xa).unsafeRunSync())
+    /*
+    generateDataSquadsTable(contentFile).foreach(i => i.run.transact(xa).unsafeRunSync())
+    generateDataPlayersTable(contentFile).foreach(i => i.run.transact(xa).unsafeRunSync())
+    generateDataTournamentsTable(contentFile2).foreach(i => i.run.transact(xa).unsafeRunSync())
+    generateDataStadiumTable(contentFile2).foreach(i => i.run.transact(xa).unsafeRunSync())
+    generateDataMatchesTable(contentFile2).foreach(i => i.run.transact(xa).unsafeRunSync())
+    generateDataTeamsTable(contentFile2).foreach(i => i.run.transact(xa).unsafeRunSync())
+    generateDataGoalsTable(contentFile2).foreach(i => i.run.transact(xa).unsafeRunSync())
+    */
 }
